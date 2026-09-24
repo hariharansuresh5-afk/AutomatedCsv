9e8511b4602a1bfac5291939214613951884d919
Your Name <your@email.com>
2026-09-24 16:01:36 +0530

    Your message is committed successfully

--- Pages/ShoppersstackMensitems.js
+++ Pages/ShoppersstackMensitems.js
@@ -7,8 +7,15 @@ export class ShoppersstackMensitems{
         this.shirts = page.locator("//a[text()='Shirts']").first();
         this.sweatshirts = page.locator("//a[text()='Sweatshirt']");
         this.sweaters = page.locator("//a[text()='Sweaters']").first();
-      /*  this.jackets = page.locator("//a[text()='Jackets']");
-        this.blazersandcoats = page.locator("//a[text()='Blazers and Coats']");*/
+        this.jackets = page.locator("//a[text()='Jackets']");
+        this.blazersandcoats = page.locator("//a[text()='Kurtas & kurta Sets']");
+        this.kurtasandsuits = page.locator("//a[text()='Sherwanis']");
+        this.EthnicWear = page.locator("//a[text()='Nehru Jakets']");
+        this.Sarees = page.locator("//a[text()='Dhotis']");
+        this.Jeans =  page.locator("//a[text()='Jeans']").first();
+     this.trouser = page.locator("//a[text()='Trousers']");
+        this.Shorts = page.locator("//a[text()='Shorts']");
+        this.TracksPantsAndJoggers = page.locator("//a[text()='Tracks Pants & Joggers']");
     }
     async shopperstackMensItems(){  
         const items = [
@@ -16,23 +23,30 @@ export class ShoppersstackMensitems{
             this.shirts,
             this.sweatshirts,
             this.sweaters,
-           /* this.jackets,
-            this.blazersandcoats*/
+            this.jackets,
+            this.blazersandcoats,
+            this.kurtasandsuits,
+            this.EthnicWear,
+            this.Sarees,        
+            this.Jeans,
+            this.trouser,
+            this.Shorts,
+            this.TracksPantsAndJoggers
         ];
 
+
+        
         for (const item of items) {
             await this.menssection.hover();
-            await this.page.waitForTimeout(500);
             await item.click();
-            await this.page.waitForLoadState('networkidle');
-            await WaitHelper.wait(this.page, 2000);
+            await this.page.waitForLoadState('domcontentloaded');
             await this.page.screenshot({
                 path: 'screenshot-' + (await item.textContent()).trim() + '.png',
                 fullPage: true
             }); 
             console.log('Loaded');
             await this.page.goBack();
-            await this.page.waitForLoadState('networkidle');
+          await this.page.waitForLoadState('domcontentloaded');
         }
     }
 }
